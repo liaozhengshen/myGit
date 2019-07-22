2cdcb75326c5b2597816162d2e60a7b82a02778b
aventador <328038075@qq.com>
2019-07-22 21:39:01 +0800

    微博漫画

---
+++ js/classify.js
@@ -0,0 +1,17 @@
+var vm=new Vue({
+	el:"#box",
+	data:{
+		classify:[{"title":"全部","type":0},{"title":"剧情","type":1},
+		{"title":"恋爱","type":2},{"title":"搞笑","type":3},{"title":"日常","type":4},
+		{"title":"总裁","type":5},{"title":"奇幻","type":6},{"title":"少年","type":7},
+		{"title":"动作","type":8},{"title":"校园","type":9},{"title":"都市","type":10},
+		{"title":"悬疑","type":11},{"title":"灵异","type":12},{"title":"古风","type":13},
+		{"title":"其他","type":14}
+		],
+		item_index:0,
+		classify_arr:[]
+	},
+	mounted(){
+		
+	}
+})
---
+++ js/classify_arr.js
@@ -0,0 +1,14 @@
+vm.$data.classify_arr=[
+	{img:"http://img.manhua.weibo.com/hcover/2018/09/12/2660188431_V9fkQA6Z.png",title:"我的奇怪少爷",desc:"我的少爷审美颠倒",type:1},
+	{img:"http://img.manhua.weibo.com/hcover/2019/04/26/6586231237_uyaomfrI.jpg",title:"百万伏特恋爱",desc:"突然成为恋爱磁石",type:1},
+	{img:"http://img.manhua.weibo.com/hcover/2018/08/22/2084322240_S0vPDff3.jpg",title:"重生特工之王者在上",desc:"穿越回古代当人质？！",type:1},
+	{img:"http://img.manhua.weibo.com/hcover/2018/09/07/2084322240_DzK0TVR5.png",title:"小猫猫给本少爷笑一个",desc:"喵星人的饲育日记",type:1},
+	{img:"http://img.manhua.weibo.com/hcover/2018/09/07/2660188431_epcQxuhN.jpg",title:"你还是毁灭地球吧",desc:"能被我看上是你的福气",type:1},
+	{img:"http://img.manhua.weibo.com/hcover/2018/09/17/2660188431_wdv4Vjwb.png",title:"金牌暗卫零零喵 ",desc:"救命，猫男把我扛上了马车…",type:1},
+	{img:"http://img.manhua.weibo.com/hcover/2018/08/21/3471934934_Hu1XqPZq.jpg",title:"三生劫",desc:"人妖情未了~",type:1},
+	{img:"http://img.manhua.weibo.com/hcover/2018/12/04/6386438008_nhkZ787l.jpg",title:"恋爱分数线",desc:" 教你如何谈恋爱！",type:1}
+	// {img:"",title:"",desc:"",type:}
+	
+
+	
+	]
